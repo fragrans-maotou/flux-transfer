59c850512258cc60b18f0af0fefcff9e75403654
maotou <1621151730@qq.com>
2025-12-21 11:57:05 +0800

    feat: initial project setup with TypeScript, tsup, vitest, and eslint

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Flux Transfer SDK - Entry Point
+ * Production-grade file transfer SDK
+ */
+
+// Core exports will be added as modules are implemented
+// export * from './core/types';
+// export * from './core/Uploader';
+// export * from './core/Downloader';
+// export * from './core/TaskQueue';
+
+// Version
+export const VERSION = '0.1.0';
